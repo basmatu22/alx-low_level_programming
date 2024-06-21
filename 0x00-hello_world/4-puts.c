c8f0f7a394588ebe85505b1f43efa99fe5f512eb
basmatu22 <hard.days11@gmail.com>
2024-06-21 08:49:24 +0300

    Add 4-puts.c program that uses puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,13 +2,10 @@
 
 /**
  * main - Entry point
- *  
  * Description: A C program that prints with puts function
- *     
  * Return: Always 0 (success)
  */
 int main(void)
-{	
-	puts("\"Programming is like building a multilingual puzzle");
+{puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
