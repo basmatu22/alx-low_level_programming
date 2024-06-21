f602a7e0792df3ec49f029a6f2cdf25b033cdbc1
basmatu22 <hard.days11@gmail.com>
2024-06-21 06:25:35 +0300

    Add 4-puts.c program that uses puts function in Betty style

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,14 @@
 #include <stdio.h>
 
 /**
-  * main - Entry point
-  *
-  * Description: A C program that prints with puts function
-  *  
-  * Return: Always 0 (success)
+ * main - Entry point
+ *  
+ * Description: A C program that prints with puts function
+ *     
+ * Return: Always 0 (success)
  */
 int main(void)
-{
-	puts("\"Programming is like building a multilingual puzzle")
+{	
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
-
