29d35909f2a166ac6e19fe528901eaf5ed32fea5
basmatu22 <hard.days11@gmail.com>
2024-06-21 07:03:50 +0300

    Fix trailing whitespace issues in 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
 /**
-  * main - Entry point
-  *   
-  * Description: A C program that prints the size of various types
-  *     
-  * Return: Always 0 (success)
-  */
+ * main - Entry point
+ *  
+ * Description: A C program that prints the size of various types
+ *     
+ * Return: Always 0 (success)
+ */
 int main(void)
-{
+{	
 	printf("Size of a char: %zu byte(s)\n", sizeof(char));
 	printf("Size of an int: %zu byte(s)\n", sizeof(int));
 	printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
