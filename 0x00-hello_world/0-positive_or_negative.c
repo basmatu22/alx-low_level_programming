784c08da91461d41fab5f108d285bf4078a820f8
basmatu22 <hard.days11@gmail.com>
2024-06-22 16:20:59 +0300

    a

--- 0x00-hello_world/0-positive_or_negative.c
+++ 0x00-hello_world/0-positive_or_negative.c
@@ -1,27 +1,24 @@
-#include <stdio.h>
-#include <stdlib.h>
+include  < stdlib.h >
 #include <time.h>
+#include <stdio.h>  /* Header for printf function */
 
 /**
- *  * main - Entry point
- *   *
- * Description: Assigns a random number to the variable n
- *                and prints whether it is positive, negative, or zero.
- * Return: Always 0 (success)
- */
+*main - Entry point  
+*Description: Assigns a random number to the variable n
+*and prints whether it is positive, negative, or zero.   
+*Return: Always 0 (success)
+*/
 int main(void)
-{
-	    int n;
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-printf("The number %d ", n);
-if(n > 0) 
-	printf("is positive\n");
-	else if (n == 0)
-	printf("is zero\n");
-	else
-	printf("is negative\n");
+{	int n;
 
-	return (0);
+	srand(time(0));  
+	n = rand() - RAND_MAX / 2;  
+	if (n > 0)
+	printf("%i is positive\n", n);
+	else if (n == 0)
+	printf("%i is zero\n", n);
+	else
+	printf("%i is negative\n", n);
+	return (0);   
 }
 
