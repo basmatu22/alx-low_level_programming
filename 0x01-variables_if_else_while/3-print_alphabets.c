590f84abffd3c90ac08ff07a2daa47e0c18ba864
basmatu22 <hard.days11@gmail.com>
2024-06-23 16:07:01 +0300

    k

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: print all alphabet letters
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	 char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+	for (ch = 'A'; ch <= 'Z'; ch++)
+	{
+
+	putchar(ch);
+
+	}
+
+	{
+	putchar('\n');
+	}
+
+	return (0);
+}
