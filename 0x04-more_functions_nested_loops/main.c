010edb84a3ee4dd71ee158bc8a839560acb30edb
basmatu22 <hard.days11@gmail.com>
2024-06-29 01:03:21 +0300

    l

--- 0x04-more_functions_nested_loops/main.c
+++ 0x04-more_functions_nested_loops/main.c
@@ -4,17 +4,12 @@
 
 int main(void)
 {
-	 char c1 = 'A';
-	 char c2 = 'a';
-	 char c3 = 'Z';
-	 char c4 = 'z';
-	 char c5 = '1';
+	int a = 10;
+	int b = 20;
+	int result;
 
-	  printf("%c: %d\n", c1, _isupper(c1));
-	  printf("%c: %d\n", c2, _isupper(c2));
-	  printf("%c: %d\n", c3, _isupper(c3));
-	  printf("%c: %d\n", c4, _isupper(c4));
-	  printf("%c: %d\n", c5, _isupper(c5));
+	result = mul(a, b);
+	printf("Multiplication of %d and %d is: %d\n", a, b, result);
 
-	  return 0;
+	return 0;
 }
