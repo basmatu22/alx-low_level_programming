fda42bbdbb5105ad4fd3c11c79d07fe84b2f0a13
basmatu22 <hard.days11@gmail.com>
2024-06-29 04:30:14 +0300

    l

--- 0x04-more_functions_nested_loops/main.c
+++ 0x04-more_functions_nested_loops/main.c
@@ -1,16 +1,13 @@
 #include "main.h"
 
-
 /**
- *main - Entry point for testing the print_line function.
+ *main - Entry point for testing the print_square function.
  *Return: 0 on success
  */
 int main(void)
 {
-	 print_line(5);  /* Expected output: _____\n */
-	 print_line(0);  /* Expected output: \n */
-	 print_line(-3); /* Expected output: \n */
-	 print_line(10); /* Expected output: ________\n */
-
-	  return (0);
+	print_square(2);
+	print_square(10);
+	print_square(0);
+	return (0);
 }
