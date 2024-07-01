2c504d29ab6922032497c3248015ce7150e1c9cf
basmatu22 <hard.days11@gmail.com>
2024-07-02 02:50:48 +0300

    h

---
+++ 0x05-pointers_arrays_strings/3-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *main - check the code
+ *
+ *Return: Always 0.
+ */
+int main(void)
+{
+	char *str;
+
+	str = "I hope you are enjoying learning C!";
+	_puts(str);
+
+	return (0);
+}
