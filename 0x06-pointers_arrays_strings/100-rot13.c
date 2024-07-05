b8f26aba5535d9689ed8c51e6f8d23017cbdba10
basmatu22 <hard.days11@gmail.com>
2024-07-05 03:30:32 +0300

    h

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ *rot13 - encodes a string using rot13
+ *@str: the string to be encoded
+ *
+ *Return: the encoded string
+ */
+char *rot13(char *str)
+{
+	int i, j;
+	char alphabet[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char rot13[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+	for (j = 0; j < 52; j++)
+	{
+	if (str[i] == alphabet[j])
+	{
+	str[i] = rot13[j];
+	break;
+	}
+	}
+	}
+	return (str);
+}
