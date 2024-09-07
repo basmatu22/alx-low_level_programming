282084d0cf287f272b5428ddb3284a7ef8f6ac37
basmatu22 <hard.days11@gmail.com>
2024-09-07 15:47:55 +0300

    l

---
+++ 0x08-recursion/1-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * main - Entry point to check the code for printing a string in reverse
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    _print_rev_recursion("\nColton Walker");
+    return (0);
+}
+
